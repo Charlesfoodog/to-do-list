2d0d2b42b8ef138fcf70cb116bb89e24d7b14b6b
Charles Sublette <charlessublette@Charless-MacBook-Pro.local>
2014-02-05 19:09:46 -0700

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AwesomeTodolist::Application.config.secret_key_base = '06aaa18ad9028b9b47c85f56b5e813b633977b8d158e6d09f4eacc3eed4b00101d000b8ce69d12b734fdbed68ac6c078571342eb2f1c5557b16248e8ccb7ba0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AwesomeTodolist::Application.config.session_store :cookie_store, key: '_awesome_todolist_session'
